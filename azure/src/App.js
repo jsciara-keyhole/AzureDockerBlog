9bd9a37c38a1187910ad26b8db6abfd683589566
jsciara-keyhole <57641671+jsciara-keyhole@users.noreply.github.com>
2020-04-28 17:35:27 -0500

    Update App.js

--- azure/src/App.js
+++ azure/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Update
+          Edit <code>src/App.js</code> and save to reload. Now what?
         </p>
         <a
           className="App-link"
