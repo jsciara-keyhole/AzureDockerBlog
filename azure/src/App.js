e6a9c114f9e3118dc81dc9ce36106a8c0de938b9
jsciara-keyhole <57641671+jsciara-keyhole@users.noreply.github.com>
2020-09-01 11:19:50 -0500

    Update App.js

--- azure/src/App.js
+++ azure/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. <br />
-          This is how we do it.
+          Continuous deployment is working.
         </p>
         <a
           className="App-link"
